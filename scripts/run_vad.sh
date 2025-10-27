43802a648c4f8b9c680a5c817cb74626f8f24db4
Ignasi Fernández Bilbeny <ignasi.fernandez.bilbeny@estudiantat.upc.edu>
2025-10-27 15:51:44 +0100

    Classe 27/10

--- scripts/run_vad.sh
+++ scripts/run_vad.sh
@@ -10,7 +10,7 @@ set -o pipefail
 # Write here the name and path of your program and database
 DIR_P2=$HOME/PAV/P2
 DB=$DIR_P2/db.v4
-CMD="$DIR_P2/bin/vad --alpha1 = $aplpha1"
+CMD="$DIR_P2/bin/vad --alpha1=$alpha1"
 
 for filewav in $DB/*/*wav; do
 #    echo
