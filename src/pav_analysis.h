cca73a05ecb788cf4cfecdf23daf73eb50625fba
Ignasi Fernández Bilbeny <ignasi.fernandez.bilbeny@estudiantat.upc.edu>
2025-11-14 19:58:49 +0100

    pav analysis

--- src/pav_analysis.h
+++ src/pav_analysis.h
@@ -1,8 +1,10 @@
 #ifndef PAV_ANALYSIS_H
 #define PAV_ANALYSIS_H
+#define M_PI 3.14159265358979323846f
 
 float compute_power(const float *x, unsigned int N);
 float compute_am(const float *x, unsigned int N);
 float compute_zcr(const float *x, unsigned int N, float fm);
+float compute_power_hamming(const float *x, unsigned int N, float fm);
 
 #endif	/* PAV_ANALYSIS_H	*/
